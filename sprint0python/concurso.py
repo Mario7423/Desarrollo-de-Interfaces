3676cf7e6ff6b96e819b59d2f347288a899a6148
Mario T <mtusetg@fpcoruna.afundacion.org>
2022-09-27 09:14:07 +0200

    Completado ejercicio 2

--- sprint0python/concurso.py
+++ sprint0python/concurso.py
@@ -1,5 +1,6 @@
 class Concurso:
 	respuesta = None
+	puntos = 0
 	print('Cuántas patas tiene un gato?')
 	print('a.1')
 	print('b.3')
@@ -8,8 +9,36 @@ class Concurso:
 	while(respuesta != 'a') & (respuesta != 'b') & (respuesta != 'c'):
 		respuesta = input('Respuesta no válida. Prueba con :(a,b,c)')
 	if (respuesta == 'a') | (respuesta == 'b'):
-		print('Respuesta incorrecta')
+		print('Respuesta incorrecta. Menos 5 puntos :/')
+		puntos -= 5
 	else:
-		print('Respuesta correcta')
-	
+		print('Respuesta correcta. Has sumado 10 puntos :3')
+		puntos += 10
+	print('Cuánto es 2 + 2?')
+	print('a.3')
+	print('b.4')
+	print('c.13')
+	respuesta = input('Selecciona tu respuesta (a,b,c)')
+	while(respuesta != 'a') & (respuesta != 'b') & (respuesta != 'c'):
+		respuesta = input('Respuesta no válida. Prueba con :(a,b,c)')
+	if (respuesta == 'a') | (respuesta == 'c'):
+		print('Respuesta incorrecta. Menos 5 puntos :/')
+		puntos -= 5
+	else:
+		print('Respuesta correcta. Has sumado 10 puntos :3')
+		puntos += 10
+	print('Cuántas manzanas le quedan a Manolito si al principio tenía 10 y luego se comió 2?')
+	print('a.8')
+	print('b.7')
+	print('c.9')
+	respuesta = input('Selecciona tu respuesta (a,b,c)')
+	while(respuesta != 'a') & (respuesta != 'b') & (respuesta != 'c'):
+		respuesta = input('Respuesta no válida. Prueba con :(a,b,c)')
+	if (respuesta == 'b') | (respuesta == 'c'):
+		print('Respuesta incorrecta. Menos 5 puntos :/')
+		puntos -= 5
+	else:
+		print('Respuesta correcta. Has sumado 10 puntos :3')
+		puntos += 10
+	print('Puntuación final = '+str(puntos))
 	
