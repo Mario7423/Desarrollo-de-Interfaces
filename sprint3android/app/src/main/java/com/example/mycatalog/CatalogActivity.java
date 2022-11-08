8e6763ffde144a91eee75a689c77705cc841f4b5
Mario T <mtusetg@fpcoruna.afundacion.org>
2022-11-08 10:11:27 +0100

    Mais errorees

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,10 +1,12 @@
 package com.example.mycatalog;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.content.Context;
 import android.content.Intent;
@@ -12,6 +14,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class CatalogActivity extends AppCompatActivity {
     private Context context = this;
@@ -23,6 +27,8 @@ public class CatalogActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
@@ -41,6 +47,43 @@ public class CatalogActivity extends AppCompatActivity {
             } else {
                 super.onBackPressed();
             }
+        }
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            int title;
+            switch (menuItem.getItemId()) {
+                case R.id.nav_camera:
+                    title = R.string.menu_camera;
+                    break;
+                case R.id.nav_gallery:
+                    title = R.string.menu_gallery;
+                    break;
+                case R.id.nav_manage:
+                    title = R.string.menu_tools;
+                    break;
+                case R.id.nav_share:
+                    title = R.string.menu_share;
+                    break;
+                case R.id.nav_send:
+                    title = R.string.menu_send;
+                    break;
+                default:
+                    throw new IllegalArgumentException("menu option not implemented!!");
+            }
+
+            Fragment fragment = HomeContentFragment.newInstance(getString(title));
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .setCustomAnimations(R.anim.nav_enter, R.anim.nav_exit)
+                    .replace(R.id.home_content, fragment)
+                    .commit();
+
+
+            setTitle(getString(title));
+
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+            return true;
         }
 
     }
