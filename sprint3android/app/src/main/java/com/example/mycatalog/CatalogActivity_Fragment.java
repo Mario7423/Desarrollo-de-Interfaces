d699faef1069df73a63ebc86eab3015166448211
Mario T <mtusetg@fpcoruna.afundacion.org>
2022-11-09 10:28:55 +0100

    Fallo compilación

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity_Fragment.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity_Fragment.java
@@ -1,30 +1,71 @@
 package com.example.mycatalog;
 
+import android.content.Intent;
+import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CatalogActivity_Fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class CatalogActivity_Fragment extends Fragment {
-    public CatalogActivity_Fragment(){}
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+
+    public CatalogActivity_Fragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @return A new instance of fragment CatalogActivity_Fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CatalogActivity_Fragment newInstance(String param1) {
+        CatalogActivity_Fragment fragment = new CatalogActivity_Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.catalog_fragment, container, false);    }
-        Button detailButton = findViewById(R.id.detailButton);
+        View layout = inflater.inflate(R.layout.catalog_fragment, container, false);
+        Button detailButton = layout.findViewById(R.id.detailButton);
         detailButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent myIntent = new Intent(context, DetailActivity.class);
-                context.startActivity(myIntent);
+                Intent myIntent = new Intent(view.getContext(), DetailActivity.class);
+                view.getContext().startActivity(myIntent);
             }
         });
+        return layout;
     }
 }
--- sprint3android/app/src/main/java/com/example/mycatalog/DesignerFragment.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DesignerFragment.java
@@ -1,21 +1,62 @@
 package com.example.mycatalog;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CatalogActivity_Fragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class DesignerFragment extends Fragment {
-    public DesignerFragment(){}
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+
+    public DesignerFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @return A new instance of fragment CatalogActivity_Fragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static DesignerFragment newInstance(String param1) {
+        DesignerFragment fragment = new DesignerFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.about_activity, container, false);    }
+        return inflater.inflate(R.layout.catalog_fragment, container, false);}
+
 }
